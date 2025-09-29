95c15a2f66b9bfa36fce80d7ed2d7b04c3a125ca
anee96935 <anee96935@gmail.com>
2025-09-29 13:53:02 +0530

    update the file 29-sep-2025-v1

--- scripts/update-brand.sh
+++ scripts/update-brand.sh
@@ -5,21 +5,37 @@ ADMIN_USER="$2"
 ADMIN_PASS="$3"
 WP_PATH="/var/www/html"
 
-# Read DB credentials from secure local files
 DB_USER=$(cat /etc/athena/db_user)
 DB_PASS=$(cat /etc/athena/db_pass)
 DB_NAME=$(cat /etc/athena/db_name)
 
-# Get public IP from Azure metadata
 NEW_IP=$(curl -s -H Metadata:true "http://169.254.169.254/metadata/instance/network/interface/0/ipv4/ipAddress/0/publicIpAddress?api-version=2021-02-01&format=text")
 
-# Update WordPress database
 mysql -u "$DB_USER" -p"$DB_PASS" "$DB_NAME" -e "
-UPDATE wp_options SET option_value = '$BRAND_NAME' WHERE option_name IN ('blogname', 'blogdescription');
-UPDATE wp_options SET option_value = 'http://$NEW_IP' WHERE option_name IN ('siteurl', 'home');
-UPDATE wp_users SET user_login = '$ADMIN_USER', user_pass = MD5('$ADMIN_PASS') WHERE ID = 1;
+UPDATE wp_options 
+SET option_value = '$BRAND_NAME' 
+WHERE option_name IN ('blogname', 'blogdescription');
+
+UPDATE wp_options 
+SET option_value = 'http://$NEW_IP' 
+WHERE option_name IN ('siteurl', 'home');
+
+UPDATE wp_users 
+SET user_login = '$ADMIN_USER', user_pass = MD5('$ADMIN_PASS') 
+WHERE ID = 1;
 "
 
-# Update WordPress options via WP-CLI
 wp option update blogname "$BRAND_NAME" --path="$WP_PATH" --allow-root
 wp option update blogdescription "$BRAND_NAME" --path="$WP_PATH" --allow-root
+
+OLD_IP="127.0.0.1"
+
+wp search-replace "http://$OLD_IP" "http://$NEW_IP" \
+  --all-tables --skip-columns=guid --dry-run \
+  --path="$WP_PATH" --allow-root
+
+wp search-replace "http://$OLD_IP" "http://$NEW_IP" \
+  --all-tables --skip-columns=guid \
+  --path="$WP_PATH" --allow-root
+
+wp cache flush --path="$WP_PATH" --allow-root
